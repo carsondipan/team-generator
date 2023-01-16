d72d47de9773c3451e7b4934394e5f00eaacbeef
Carson DiPangrazio <carson.dipangrazio@gmail.com>
2023-01-16 18:27:24 -0500

    message

--- Develop/index.js
+++ Develop/index.js
@@ -6,8 +6,79 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Employee = require('./lib/Employee');
 
+const pageTemplate = require('./src/page-template')
+
 const prompt = inquirer.createPromptModule();
 const teamMembers = [];
+
+const managerQuestions = [
+    {
+        message: 'Who is your team Manager?',
+        name: 'name',
+        default: 'John',
+    },
+    {
+        message: 'What is the ID of the Manager?',
+        name: 'id',
+        default: '123',
+    },
+    {
+        message: 'What is the email of your Manager?',
+        name: 'email',
+        default: 'john@email.com',
+    },
+    {
+        message: 'What office is your team manager??',
+        name: 'officeNumber',
+        default: '1234',
+    },
+];
+
+const engineerQuestions = [
+    {
+        message: 'Who is your Engineer?',
+        name: 'name',
+        default: 'Rob',
+    },
+    {
+        message: 'What is the ID of the Engineer?',
+        name: 'id',
+        default: '07',
+    },
+    {
+        message: 'What is the email of your Engineer?',
+        name: 'email',
+        default: 'rob07@email.com',
+    },
+    {
+        message: 'What is the GitHub of your engineer?',
+        name: 'gitHub',
+        default: 'rob07',
+    },
+];
+
+const internQuestions = [
+    {
+        message: 'What is your interns name',
+        name: 'name',
+        default: 'Scum',
+    },
+    {
+        message: 'What is the ID of your intern?',
+        name: 'id',
+        default: '420',
+    },
+    {
+        message: 'What is the email of your intern?',
+        name: 'email',
+        default: 'scum20@email.com',
+    },
+    {
+        message: 'What school is your intern from?',
+        name: 'school',
+        default: 'ECU',
+    },
+];
 
 const chooseEmployeeType = async ({ type }) => {
     let response;
@@ -35,72 +106,6 @@ const chooseEmployeeType = async ({ type }) => {
         }
     }
 };
-const managerQuestions = [
-    {
-        message: 'Who is your team Manager?',
-        name: 'name',
-        default: 'John',
-    },
-    {
-        message: 'What is the ID of the Manager?',
-        name: 'id',
-        default: '123',
-    },
-    {
-        message: 'What is the email of your Manager?',
-        name: 'email',
-        default: 'john@email.com',
-    },
-    {
-        message: 'What office is your team manager??',
-        name: 'officeNumber',
-        default: '1234',
-    },
-];
-const engineerQuestions = [
-    {
-        message: 'Who is your Engineer?',
-        name: 'name',
-        default: 'Rob',
-    },
-    {
-        message: 'What is the ID of the Engineer?',
-        name: 'id',
-        default: '07',
-    },
-    {
-        message: 'What is the email of your Engineer?',
-        name: 'email',
-        default: 'rob07@email.com',
-    },
-    {
-        message: 'What is the GitHub of your engineer?',
-        name: 'gitHub',
-        default: 'rob07',
-    },
-];
-const internQuestions = [
-    {
-        message: 'What is your interns name',
-        name: 'name',
-        default: 'Scum',
-    },
-    {
-        message: 'What is the ID of your intern?',
-        name: 'id',
-        default: '420',
-    },
-    {
-        message: 'What is the email of your intern?',
-        name: 'email',
-        default: 'scum20@email.com',
-    },
-    {
-        message: 'What school is your intern from?',
-        name: 'school',
-        default: 'ECU',
-    },
-];
 
 const confirmMoreEmployees = () => {
     return prompt({
@@ -109,11 +114,12 @@ const confirmMoreEmployees = () => {
         name: 'addMore',
     })
 };
-const addMoreEmployees = () => {
+const addMoreEmployees = ({ addMore }) => {
     if (addMore) {
         console.log('CONTINUE');
     } else {
-        console.log('WRITE FILE');
+        const template = pageTemplate(teamMembers);
+        fs.writeFileSync('./dist/team.html', template);
     }
 };
 
@@ -122,34 +128,19 @@ prompt(managerQuestions)
         const manager = new Manager(name, id, email, officeNumber);
         teamMembers.push(manager);
     })
-    .then(() => {
-        prompt({
-            message: "Would you like to add more employees?",
-            type: 'confirm',
-            name: 'addMore',
-        })
-    })
-    .then(({ addMore }) => {
-        if (addMore) {
-            console.log('CONTINUE');
-        } else {
-            console.log('WRITE FILE');
-        }
-    })
+    .then(confirmMoreEmployees)
+    .then(addMoreEmployees)
     .then(() => {
         return prompt({
             type: 'rawlist',
-            message: 'What kind of employee would you like to add?',
+            message: 'What kind of employee do you want to add?',
             choices: [
-                'Intern',
                 'Engineer',
+                'Intern',
                 'Manager',
             ],
-            name: 'type'
         })
     })
     .then(chooseEmployeeType)
-    // .then((Employee) => {
-    //     teamMembers(employee);
-    //     const employee = new 
-    // })
+    .then(confirmMoreEmployees)
+    .then(addMoreEmployees)
